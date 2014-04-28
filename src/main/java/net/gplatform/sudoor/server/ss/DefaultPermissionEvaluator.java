62165c2a9609e3c53fd50cc355d95a33a84d625f
Shark <xfcjscn@gmail.com>
2014-04-28 21:21:45 +0800

    convert object field to class field

--- src/main/java/net/gplatform/sudoor/server/ss/DefaultPermissionEvaluator.java
+++ src/main/java/net/gplatform/sudoor/server/ss/DefaultPermissionEvaluator.java
@@ -21,10 +21,10 @@ import java.util.Properties;
  * Created by Administrator on 14-1-15.
  */
 public class DefaultPermissionEvaluator implements PermissionEvaluator {
-	final Logger logger = LoggerFactory.getLogger(DefaultPermissionEvaluator.class);
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultPermissionEvaluator.class);
 
-	public final String CONFIG_FILE = "PermissionEvaluator/PermissionEvaluator.properties";
-	public final String CONFIG_EXPRESSION_PREFIX = "PermissionEvaluator.expression.";
+	public static final String CONFIG_FILE = "PermissionEvaluator/PermissionEvaluator.properties";
+	public static final String CONFIG_EXPRESSION_PREFIX = "PermissionEvaluator.expression.";
 
 	private Properties configProperties = new Properties();
 
@@ -33,19 +33,19 @@ public class DefaultPermissionEvaluator implements PermissionEvaluator {
 	private Map<String, Map<Object, Expression>> expressions = new HashMap<String, Map<Object, Expression>>();
 
 	public DefaultPermissionEvaluator() {
-		logger.debug("Start to init DefaultPermissionEvaluator");
+		LOG.debug("Start to init DefaultPermissionEvaluator");
 		try {
 			InputStream is = this.getClass().getClassLoader().getResourceAsStream(CONFIG_FILE);
 			configProperties.load(is);
-			logger.debug("Successfully Init DefaultPermissionEvaluator");
+			LOG.debug("Successfully Init DefaultPermissionEvaluator");
 		} catch (Exception e) {
-			logger.error("Can not init DefaultPermissionEvaluator", e);
+			LOG.error("Can not init DefaultPermissionEvaluator", e);
 		}
 	}
 
 	/**
 	 * Find the expression string recursively from the config file
-	 *
+	 * 
 	 * @param name
 	 * @param permission
 	 * @return
@@ -64,10 +64,9 @@ public class DefaultPermissionEvaluator implements PermissionEvaluator {
 			expression = getExpressionString(parent, permission);
 		}
 
-		logger.debug("Get Expression String: [{}] for name: [{}] permission: [{}]", expression, name, permission);
+		LOG.debug("Get Expression String: [{}] for name: [{}] permission: [{}]", expression, name, permission);
 		return expression;
 	}
-
 
 	@Override
 	public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
@@ -85,7 +84,7 @@ public class DefaultPermissionEvaluator implements PermissionEvaluator {
 				expression = parser.parseExpression(expressionString);
 			} else {
 				expression = parser.parseExpression("false");
-				logger.warn("No Expression configed for name: [{}] permission: [{}], Default to false Expression!", name, permission);
+				LOG.warn("No Expression configed for name: [{}] permission: [{}], Default to false Expression!", name, permission);
 			}
 			m.put(permission, expression);
 		}
@@ -96,16 +95,16 @@ public class DefaultPermissionEvaluator implements PermissionEvaluator {
 		boolean res = false;
 		try {
 			res = expression.getValue(context, Boolean.class);
-			logger.debug("Evaluate Result : [{}] for name: [{}] permission: [{}]", res, name, permission);
+			LOG.debug("Evaluate Result : [{}] for name: [{}] permission: [{}]", res, name, permission);
 		} catch (Exception e) {
-			logger.error("Error parse expression", e);
+			LOG.error("Error parse expression", e);
 		}
 		return res;
 	}
 
 	@Override
 	public boolean hasPermission(Authentication authentication, Serializable serializable, String s, Object o) {
-		logger.error("Not implemented!!!");
+		LOG.error("Not implemented!!!");
 		return false;
 	}
 }
