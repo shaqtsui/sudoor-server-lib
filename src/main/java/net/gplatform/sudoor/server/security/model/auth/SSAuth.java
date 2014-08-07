d09eeee2ca0a44e6f735fbc662b50d63a546dbdf
xufucheng <xfcjscn@gmail.com>
2014-08-07 11:58:59 +0800

    ssauth

--- src/main/java/net/gplatform/sudoor/server/security/model/auth/SSAuth.java
+++ src/main/java/net/gplatform/sudoor/server/security/model/auth/SSAuth.java
@@ -1,6 +1,7 @@
 package net.gplatform.sudoor.server.security.model.auth;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import net.gplatform.sudoor.server.security.model.entity.CredentialAuthority;
@@ -15,6 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 @Component
 public class SSAuth {
@@ -35,26 +37,74 @@ public class SSAuth {
 	}
 
 	public String register(String username, String password) {
-		return register(username, password, "ROLE_USER");
+		return register(username, password, new String[] { "ROLE_USER" });
 	}
 
-	public String register(String username, String password, String role) {
+	public String register(String username, String password, String[] roles) {
 		logger.debug("Register:" + username);
 
 		CredentialUser credentialUser = new CredentialUser();
 		credentialUser.setUsername(username);
 		credentialUser.setPassword(password);
 		credentialUser.setEnabled(true);
-
-		CredentialAuthority credentialAuthority = new CredentialAuthority();
-		credentialAuthority.setUsername(username);
-		credentialAuthority.setAuthority(role);
-		List<CredentialAuthority> credentialAuthorities = new ArrayList<CredentialAuthority>();
-		credentialAuthorities.add(credentialAuthority);
-
-		credentialUser.setCredentialAuthorities(credentialAuthorities);
+		credentialUser.setCredentialAuthorities(createCredentialAuthorities(username, roles));
 
 		credentialUserRepository.saveAndFlush(credentialUser);
 		return "SUCCESS";
+	}
+
+	public String updatePassword(String username, String password) {
+		CredentialUser credentialUser = credentialUserRepository.findOne(username);
+		credentialUser.setPassword(password);
+		credentialUserRepository.saveAndFlush(credentialUser);
+		return "SUCCESS";
+	}
+
+	public String disableUser(String username) {
+		CredentialUser credentialUser = credentialUserRepository.findOne(username);
+		credentialUser.setEnabled(false);
+		credentialUserRepository.saveAndFlush(credentialUser);
+		return "SUCCESS";
+	}
+
+	public String enableUser(String username) {
+		CredentialUser credentialUser = credentialUserRepository.findOne(username);
+		credentialUser.setEnabled(true);
+		credentialUserRepository.saveAndFlush(credentialUser);
+		return "SUCCESS";
+	}
+
+	public String addRole(String username, String[] roles) {
+		CredentialUser credentialUser = credentialUserRepository.findOne(username);
+		credentialUser.setCredentialAuthorities(createCredentialAuthorities(username, roles));
+		credentialUserRepository.saveAndFlush(credentialUser);
+		return "SUCCESS";
+	}
+
+	public String removeRole(String username, String[] roles) {
+		CredentialUser credentialUser = credentialUserRepository.findOne(username);
+		List<CredentialAuthority> credentialAuthorities = credentialUser.getCredentialAuthorities();
+		Iterator rolesIterator = CollectionUtils.arrayToList(roles).iterator();
+
+		for (Iterator iterator = credentialAuthorities.iterator(); iterator.hasNext();) {
+			CredentialAuthority credentialAuthority = (CredentialAuthority) iterator.next();
+			if (CollectionUtils.contains(rolesIterator, credentialAuthority.getAuthority())) {
+				iterator.remove();
+			}
+		}
+		credentialUserRepository.saveAndFlush(credentialUser);
+		return "SUCCESS";
+	}
+
+	private List<CredentialAuthority> createCredentialAuthorities(String username, String[] roles) {
+		List<CredentialAuthority> credentialAuthorities = new ArrayList<CredentialAuthority>();
+		for (int i = 0; i < roles.length; i++) {
+			String role = roles[i];
+			CredentialAuthority credentialAuthority = new CredentialAuthority();
+			credentialAuthority.setUsername(username);
+			credentialAuthority.setAuthority(role);
+			credentialAuthorities.add(credentialAuthority);
+		}
+		return credentialAuthorities;
 	}
 }
