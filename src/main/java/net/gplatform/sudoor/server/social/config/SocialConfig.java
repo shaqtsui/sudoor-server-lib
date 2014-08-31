4db124130a24b8b912a1df92bc9023b6994fca0d
xufucheng <xfcjscn@gmail.com>
2014-08-31 09:31:18 +0800

    config signincontroller signup url

--- src/main/java/net/gplatform/sudoor/server/social/config/SocialConfig.java
+++ src/main/java/net/gplatform/sudoor/server/social/config/SocialConfig.java
@@ -2,9 +2,11 @@ package net.gplatform.sudoor.server.social.config;
 
 import net.gplatform.sudoor.server.social.model.signin.SimpleSignInAdapter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.social.connect.web.ProviderSignInController;
 import org.springframework.social.connect.web.SignInAdapter;
 
 @Configuration
@@ -13,5 +15,10 @@ public class SocialConfig{
 	@Bean
 	public SignInAdapter signInAdapter() {
 		return new SimpleSignInAdapter(new HttpSessionRequestCache());
+	}
+	
+	@Autowired
+	public void configProviderSignInController(ProviderSignInController providerSignInController){
+		providerSignInController.setSignUpUrl("/app/signup");
 	}
 }
