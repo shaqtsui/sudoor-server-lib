4a08c8d83d52902dbbcbb5a57895d27c2d32cbbf
xufucheng <xfcjscn@gmail.com>
2014-12-08 17:40:52 +0800

    test

--- src/test/java/net/gplatform/sudoor/server/test/it/SSAuthTest.java
+++ src/test/java/net/gplatform/sudoor/server/test/it/SSAuthTest.java
@@ -1,8 +1,15 @@
 package net.gplatform.sudoor.server.test.it;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import net.gplatform.sudoor.server.Application;
 import net.gplatform.sudoor.server.security.model.auth.SSAuth;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +23,22 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 @IntegrationTest
 public class SSAuthTest {
+	public final String REST_SERVICE_URL = "http://localhost:8080/product/data/ws/rest";
+
+	public final String ODATA_SERVICE_URL = "http://localhost:8080/product/data/odata.svc";
+
+	static Client client = null;
+
+
 	@Autowired
 	SSAuth SSAuth;
+	
+	
+	@BeforeClass
+	public static void  init() {
+		//register(JacksonFeatures.class).
+		client = ClientBuilder.newBuilder().build();
+	}
 	
 	@Test
 	public void test() {
@@ -53,4 +74,19 @@ public class SSAuthTest {
 	public void testUserDetailsService() {
 		boolean res = SSAuth.getUserDetailsManager().userExists("Shark");
 	}
+	
+	@Test
+	public void testSpringSecurity() {
+		WebTarget target = client.target(REST_SERVICE_URL).path("/sudoor/SpringSecurity/Authentication");
+		Response response = target.request(MediaType.WILDCARD_TYPE).get();
+		int statusCode = response.getStatus();
+		String content = response.readEntity(String.class);
+
+		System.out.println("retrieveFile1() statusCode:" + statusCode);
+		// System.out.println("retrieveFile1() content:" + content);
+		assert (statusCode == 200);
+	}
+	
+	
+	
 }
