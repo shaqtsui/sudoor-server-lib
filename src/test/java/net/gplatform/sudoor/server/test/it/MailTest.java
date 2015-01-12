033373c72e6f44af893e7bb8703b2e6e96ae79e4
xufucheng <xfcjscn@gmail.com>
2015-01-12 18:00:39 +0800

    add starter mail

--- src/test/java/net/gplatform/sudoor/server/test/it/MailTest.java
+++ src/test/java/net/gplatform/sudoor/server/test/it/MailTest.java
@@ -1,15 +1,17 @@
 package net.gplatform.sudoor.server.test.it;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import net.gplatform.sudoor.server.Application;
-import net.gplatform.sudoor.server.mail.MailClient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -17,18 +19,20 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 @IntegrationTest
-public class MailClientTest {
+public class MailTest {
 
 	@Autowired
-	MailClient mailClient;
+	JavaMailSender mailSender;
 
 	@Test
 	public void test() throws MessagingException {
-		try {
-			mailClient.send("xufucheng@vcredit.com", "我");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+		mimeMessageHelper.setSubject("test");
+		mimeMessageHelper.setTo("xufucheng@vcredit.com");
+		mimeMessageHelper.setFrom("xufucheng@vcredit.com");
+		mimeMessageHelper.setText( "我", false);
+		mailSender.send(mimeMessage);
 	}
 
 }
