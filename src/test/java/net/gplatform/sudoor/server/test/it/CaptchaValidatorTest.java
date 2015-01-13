9ac9f41b23aede2be6843c8c6160ef720ce0dd2c
xufucheng <xfcjscn@gmail.com>
2015-01-13 12:47:46 +0800

    captcha test

--- src/test/java/net/gplatform/sudoor/server/test/it/CaptchaValidatorTest.java
+++ src/test/java/net/gplatform/sudoor/server/test/it/CaptchaValidatorTest.java
@@ -1,9 +1,15 @@
 package net.gplatform.sudoor.server.test.it;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 import net.gplatform.sudoor.server.Application;
@@ -36,13 +42,25 @@ public class CaptchaValidatorTest {
 	}
 
 	@Test
-	public void validateCaptcha() {
-		WebTarget target = client.target(testUtils.getEmbeddedServletContainerBaseURL() + "/data/ws/rest").path("/sudoor/captcha/validate").queryParam("_captcha", "abc");
-		Response response = target.request(MediaType.WILDCARD_TYPE).get();
-		boolean res = response.readEntity(boolean.class);
-		System.out.println(res);
-		int statusCode = response.getStatus();
-		assert (statusCode == 200);
+	public void validateCaptchaFalse() {
+
+		WebTarget captchaImgTarget = client.target(testUtils.getEmbeddedServletContainerBaseURL() + "/app/sudoor/captcha-image.html");
+		Response captchaImgResponse = captchaImgTarget.request(MediaType.WILDCARD_TYPE).get();
+		Map<String, NewCookie> captchaImgCookies = captchaImgResponse.getCookies();
+		assert (captchaImgResponse.getStatus() == 200);
+
+		String pageCaptcha = "abc";
+		WebTarget validateTarget = client.target(testUtils.getEmbeddedServletContainerBaseURL() + "/data/ws/rest").path("/sudoor/captcha/validate")
+				.queryParam("_captcha", pageCaptcha);
+		Builder validateBuilder = validateTarget.request(MediaType.WILDCARD_TYPE);
+		for (Iterator iterator = captchaImgCookies.values().iterator(); iterator.hasNext();) {
+			Cookie cookie = (Cookie) iterator.next();
+			validateBuilder.cookie(cookie);
+		}
+		Response validateResponse = validateBuilder.get();
+		assert (validateResponse.getStatus() == 200);
+
+		boolean res = validateResponse.readEntity(boolean.class);
 		assert (res == false);
 	}
 
