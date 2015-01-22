8759ef20ad6f79999f16e72ea45019170c6156de
xufucheng <xfcjscn@gmail.com>
2015-01-22 13:20:32 +0800

    add EclipselinkJpaAutoConfiguration

---
+++ src/main/java/net/gplatform/sudoor/server/jpa/autoconfig/EclipselinkJpaAutoConfiguration.java
@@ -0,0 +1,72 @@
+package net.gplatform.sudoor.server.jpa.autoconfig;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import net.gplatform.sudoor.server.jpa.autoconfig.EclipselinkJpaAutoConfiguration.EclipselinkEntityManagerCondition;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jta.JtaAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.ClassUtils;
+
+@Configuration
+@ConditionalOnClass({ LocalContainerEntityManagerFactoryBean.class, EnableTransactionManagement.class, EntityManager.class })
+@Conditional(EclipselinkEntityManagerCondition.class)
+@AutoConfigureAfter({ DataSourceAutoConfiguration.class, JtaAutoConfiguration.class })
+public class EclipselinkJpaAutoConfiguration extends JpaBaseConfiguration {
+
+	@Autowired
+	private JpaProperties properties;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+		EclipseLinkJpaVendorAdapter adapter = new EclipseLinkJpaVendorAdapter();
+		return adapter;
+	}
+
+	@Override
+	protected Map<String, Object> getVendorProperties() {
+		Map<String, Object> vendorProperties = new LinkedHashMap<String, Object>();
+		return vendorProperties;
+	}
+
+	@Order(Ordered.HIGHEST_PRECEDENCE + 20)
+	static class EclipselinkEntityManagerCondition extends SpringBootCondition {
+
+		private static String[] CLASS_NAMES = { "org.eclipse.persistence.jpa.JpaEntityManager" };
+
+		@Override
+		public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
+			for (String className : CLASS_NAMES) {
+				if (ClassUtils.isPresent(className, context.getClassLoader())) {
+					return ConditionOutcome.match("found EclipselinkEntityManager class");
+				}
+			}
+			return ConditionOutcome.noMatch("did not find EclipselinkEntityManager class");
+		}
+	}
+
+}
