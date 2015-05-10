b9277f7a8a53e669818eee38d7e36169b333b94c
Shark <xfcjscn@gmail.com>
2015-05-10 16:19:25 +0800

    flywaydb DB initialize replace Spring JDBC DB initialize (spring.datasource.initialize)

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users VALUES ('admin', '$2a$10$1C./yRXWExLyay1ccRa3SeMedkW0/Tjv18P7jBxu1unGAScWSHB4m',true);
-INSERT INTO authorities VALUES ('admin', 'ROLE_ADMIN');
--- src/main/resources/db/migration/V0_0_1__SpringSecurity.sql
+++ src/main/resources/db/migration/V0_0_1__SpringSecurity.sql
@@ -9,4 +9,9 @@ create table authorities (
     authority varchar_ignorecase(50) not null,
     constraint fk_authorities_users foreign key(username) references users(username)
 );
+
 create unique index ix_auth_username on authorities (username,authority);
+
+
+INSERT INTO users VALUES ('admin', '$2a$10$1C./yRXWExLyay1ccRa3SeMedkW0/Tjv18P7jBxu1unGAScWSHB4m',true);
+INSERT INTO authorities VALUES ('admin', 'ROLE_ADMIN');
