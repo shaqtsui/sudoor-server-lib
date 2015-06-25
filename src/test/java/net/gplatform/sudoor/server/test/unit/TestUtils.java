397abe14e83136338735a138b8689abfea53b6c5
xufucheng <xfcjscn@gmail.com>
2015-06-25 15:26:13 +0800

    add takeScreenshot, spring.redis.host

--- src/test/java/net/gplatform/sudoor/server/test/unit/TestUtils.java
+++ src/test/java/net/gplatform/sudoor/server/test/unit/TestUtils.java
@@ -22,6 +22,8 @@ package net.gplatform.sudoor.server.test.unit;
  * #L%
  */
 
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Iterator;
 
@@ -29,6 +31,10 @@ import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.AbstractConfigurableEmbeddedServletContainer;
 import org.springframework.stereotype.Component;
@@ -51,12 +57,27 @@ public class TestUtils {
 
 		return "http://" + addressStr + ":" + port + contextPath;
 	}
-	
-	
+
 	public static void copyCookies(Builder targetRequstBuilder, Response fromResponse) {
 		for (Iterator iterator = fromResponse.getCookies().values().iterator(); iterator.hasNext();) {
 			Cookie cookie = (Cookie) iterator.next();
 			targetRequstBuilder.cookie(cookie);
+		}
+	}
+
+	/**
+	 * Take screen short & save to: target/test/selenium/
+	 * 
+	 * @param driver
+	 * @param name
+	 */
+	public static void takeScreenshot(WebDriver driver, String name) {
+		byte[] bScreenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+		File target = new File("target/test/selenium/" + name);
+		try {
+			FileUtils.writeByteArrayToFile(target, bScreenshot);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
