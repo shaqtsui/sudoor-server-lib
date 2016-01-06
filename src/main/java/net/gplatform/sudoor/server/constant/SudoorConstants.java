027b416ec44379dc9cb3ed0f4fafd3d50c1eb741
xfcjscn <xfcjscn@163.com>
2016-01-06 13:20:03 +0800

    add comments: 	Env Dependency: 		Development: 			webdriver - for integration test 		Runtime: 			N.A. Remove cors as spring boot 1.3 already provided related support

--- src/main/java/net/gplatform/sudoor/server/constant/SudoorConstants.java
+++ src/main/java/net/gplatform/sudoor/server/constant/SudoorConstants.java
@@ -4,7 +4,6 @@ public class SudoorConstants {
 	/**
 	 * Default Spring Security Filter Chain filter order is 0.
 	 */
-	public static final int ORDER_FILTER_CORS = -1000;
 	
 	public static final int ORDER_FILTER_MDC_INSERTING = -3000;
 	
